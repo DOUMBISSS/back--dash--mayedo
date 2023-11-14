d8aa0e0a1e2a1f84e33c0839dd002cc04c547f82
DOUMBISSS <104175220+DOUMBISSS@users.noreply.github.com>
2023-11-14 19:36:42 +0000

    maj

--- db/database.js
+++ db/database.js
@@ -1,8 +1,8 @@
 import mongoose from "mongoose";
 
 // const server = '127.0.0.1:27017'; 
-// const server = 'mongodb+srv://doumbisss:Djenebou77@cluster0.txac1je.mongodb.net/mayedo?retryWrites=true&w=majority'
-const server = 'mongodb+srv://doumbia77fode:Djenebou77@clustermayedo.jcisxbu.mongodb.net/'
+const server = 'mongodb+srv://doumbia77fode:Djenebou77@clustermayedo.jcisxbu.mongodb.net/?retryWrites=true&w=majority'
+// const server = 'mongodb+srv://doumbia77fode:Djenebou77@clustermayedo.jcisxbu.mongodb.net/'
 const database = 'dash mayedo';     
 class Database {
     static connect() {
