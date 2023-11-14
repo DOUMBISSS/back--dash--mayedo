7f20ec59064af5d6339c3e4ba09698ac63122fad
DOUMBISSS <104175220+DOUMBISSS@users.noreply.github.com>
2023-11-14 19:10:23 +0000

    maj

--- routes/user.js
+++
@@ -1,8 +0,0 @@
-const express = require("express")
-const router = express.Router()
-const userCtrl = require('../controller/user')
-
-router.all('/signup',userCtrl.signup )
-router.all('/login' , userCtrl.login)
-
-module.exports = router
